8f720cb3758466501106facfd5a426d254135f93
teeaychem <ben@sparkes.uk.net>
2023-09-16 17:06:08 +0100

    Symbolic constants

--- chapter 1/fahr-cel-table-for.c
+++ chapter 1/fahr-cel-table-for.c
@@ -4,6 +4,20 @@
   make a Fahrenheit-Celcuis table
  */
 
+# define LOWER 0
+# define FUPPER 212
+# define FSTEP 20
+# define CUPPER 100
+# define CSTEP 7.532
+/* No typing with symbolic constants… */
+
+# define CLOSING_MSG "Goodbye!\n"
+/*
+  What's stored is determined.
+  But, what about interpreting the bits?
+ */
+
+
 int main() {
   /* int fahr, celsius; */
   float celsius, fahr;
@@ -15,7 +29,7 @@ int main() {
     No restriction on test condition. But need everything initialised.
     So, test before first instance, allowing to skip entirely.
   */
-  for (fahr = 212; celsius >= 0; fahr = fahr - 20) {
+  for (fahr = FUPPER; celsius >= LOWER; fahr = fahr - FSTEP) {
 
     celsius = (5 * (fahr - 32)) / 9.0;
     printf("%6.2f < c ~ f > %6.2f\n", celsius, fahr);
@@ -23,8 +37,10 @@ int main() {
 
   printf("And now, for some celcuis\n");
 
-  for (celsius = 100; celsius >= 0; celsius = celsius - 5) {
+  for (celsius = CUPPER; celsius >= LOWER; celsius = celsius - CSTEP) {
     fahr = ((celsius * 9) / 5.0) + 32;
     printf("%6.2f < c ~ f > %6.2f\n", celsius, fahr);
   }
+
+  printf(CLOSING_MSG);
 }
