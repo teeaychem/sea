9dcab7ea4151869f7647ffb3adf3c2f66d7bc236
teeaychem <ben@sparkes.uk.net>
2023-09-16 23:20:15 +0100

    Ex. 1.7

--- chapter 1/1.7.c
+++ chapter 1/1.7.c
@@ -1,9 +1,15 @@
-  #include <stdio.h>
+#include <stdio.h>
 
 int val;
 char c;
 
 int main() {
   val = ((c = getchar()) != EOF);
-  printf("%d", val);
+  printf("You entered %c\n" , c);
+  printf("Testing whether %c is not equal to EOF returns %d\n", c, val);
+
+  printf("Also, if we try to print EOF the result is:\n");
+  printf("%c - with c\n", EOF);
+  printf("%d - with d\n", EOF);
+  printf("Goodbye!\n", EOF);
 }
