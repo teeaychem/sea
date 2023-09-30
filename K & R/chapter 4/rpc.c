24b3a431dc9e68b601840fccf85aa2907d6cdb68
teeaychem <ben@sparkes.uk.net>
2023-09-30 11:40:09 +0100

    EOF?

--- K & R/chapter 4/rpc.c
+++ K & R/chapter 4/rpc.c
@@ -172,15 +172,15 @@ int getop(char s[])
 }
 
 #define BUFSIZE 100
-char buf[BUFSIZE];    /* buffer for ungetch */
+int buf[BUFSIZE];    /* buffer for ungetch */
 int bufp = 0;         /* next free position in buf */
 
-int getch(void)  /* get a (possibly pushed−back) character */
+int getch(void)  /* get a (possibly pushed−back) result of getchar */
 {
   return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-void ungetch(int c)   /* push character back on input */
+void ungetch(int c)   /* push result of getchar back on input */
 {
   if (bufp >= BUFSIZE)
     printf("ungetch: too many characters\n");
