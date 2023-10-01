006415872c7adde8fac323a9157b4e93a7ca9a5b
teeaychem <ben@sparkes.uk.net>
2023-10-01 18:00:54 +0100

    Addiotnal swap macro investigation

--- K & R/chapter 4/swap.c
+++ K & R/chapter 4/swap.c
@@ -2,17 +2,27 @@
 
 #define swp(t,x,y) { t swap = x; x = y; y = swap; }
 
+#define swap(t,x,y) { t c = x; t d = y; t swap = c; c = d; d = swap; x = c; y = d; }
+
 int main()
 {
   int x = 1;
   int y = 0;
   printf("x: %d y: %d\n", x, y);
-  swp(int, x, y);
+  swap(int, x, y);
   printf("x: %d y: %d\n", x, y);
 
-  double a = 432;
-  double b = 643543534543656547 * 5;
-  printf("a: %g b: %g\n", a, b);
-  swp(double, a, b);
-  printf("a: %g b: %g\n", a, b);
+  int c = 1;
+  int d = 0;
+  printf("c: %d d: %d\n", c, d);
+  swap(int, c, d);
+  printf("c: %d d: %d\n", c, d);
+  swp(int, c, d);
+  printf("c: %d d: %d\n", c, d);
+
+  int a = 432;
+  int swap = 234;
+  printf("a: %d b: %d\n", a, swap);
+  swap(int, a, swap);
+  printf("a: %d b: %d\n", a, swap);
 }
