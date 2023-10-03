5c803898640b54973232cd7beb714e1a3eead2c6
teeaychem <ben@sparkes.uk.net>
2023-10-03 10:20:13 +0100

    Rename and added strend

--- K & R/chapter 5/str_ptr.c
+++ K & R/chapter 5/str_ptr.c
@@ -4,10 +4,14 @@ int main(void)
 {
   void test_strcat_ptr(void);
   void test_strcat_book(void);
+  void test_strend(void);
 
   test_strcat_ptr();
 
   test_strcat_book();
+
+  test_strend();
+
   return 0;
 }
 
@@ -69,4 +73,54 @@ void strcat_book(char s[], char t[])
     i++;
   while ((s[i++] = t[j++]) != '\0') /* copy t */
     ;
+}
+
+void test_strend(void)
+{
+  printf("\nTesting: strend\n");
+  void strcat_ptr(char *s, char *t);
+  int strend(char *s, char *t);
+  char s[15] = "Hello, "; // ensure s is big enough
+  char t[5] = "world";
+
+  strcat_ptr(s, t);
+
+  printf("\tStrings:\n"
+	 "\ts:\t%s\n"
+	 "\tt:\t%s\n", s, t);
+
+  printf("\tIs t is at the end of s? %d\n", strend(s, t));
+  printf("\tIs s is at the end of t? %d\n", strend(t, s));
+}
+
+/* strend: return 1 if t is at the end of s, 0 otherwise */
+int strend(char *s, char *t)
+{
+  int slen = 0;
+  int tlen = 0;
+
+  while (*s != '\0') { /* find the end of s */
+    s++;
+    slen++;
+  }
+
+  while (*t != '\0') { /* find the end of s */
+    t++;
+    tlen++;
+  }
+
+  /* work backwards checking char equality */
+  while (*s == *t && tlen >= 0 && slen >= 0) {
+    s--;
+    t--;
+    slen--;
+    tlen--;
+  }
+
+  /* return 1 only if walked back all of t */
+  if (tlen == -1) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
