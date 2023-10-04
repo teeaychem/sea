b809449398a7847582caf7d6950c3e196b66835b
teeaychem <ben@sparkes.uk.net>
2023-10-04 09:34:34 +0100

    alloc incorporated into main/readlines

--- K & R/chapter 5/sort.c
+++ K & R/chapter 5/sort.c
@@ -4,19 +4,22 @@
 #include "./qsort.c"
 
 #define MAXLINES 5000 /* max #lines to be sorted */
+#define ALLOCSIZE 10000 /* size of available space */
 
 char *lineptr[MAXLINES]; /* pointers to text lines */
 
-int readlines(char *lineptr[], int nlines);
+int readlines_main(char *lineptr[], int maxlines, char buf[]);
 void writelines(char *lineptr[], int nlines);
 
 void qsort(char *lineptr[], int left, int right);
 
 /* sort input lines */
 int main() {
-  int nlines; /* number of input lines read */
 
-  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+  int nlines; /* number of input lines read */
+  char buf[ALLOCSIZE];
+
+  if ((nlines = readlines_main(lineptr, MAXLINES, buf)) >= 0) {
     qsort(lineptr, 0, nlines - 1);
     writelines(lineptr, nlines);
     return 0;
@@ -28,12 +31,36 @@ int main() {
 }
 
 #define MAXLEN 1000 /* max length of any input line */
-int getlinealt(char *, int);
-char *alloc(int);
+
+
+int readlines_main(char *lineptr[], int maxlines, char buf[])
+{
+  int getlinealt(char *, int);
+
+  char *bufp = buf; /* buf only updated within readlines */
+  int len;
+  char line[MAXLEN];
+  int nlines = 0;
+
+ while ((len = getlinealt(line, MAXLEN)) > 0)
+   /* replicate free mem test inline */
+   if (nlines >= maxlines || (buf + ALLOCSIZE - bufp < len))
+     return -1;
+   else {
+     line[len - 1] = '\0'; /* delete newline */
+     lineptr[nlines++] = bufp; /* store ptr to line */
+     strcpy(bufp, line);
+     bufp += len; /* update bufp past stored line */
+   }
+ return nlines;
+}
 
 /* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines)
+int readlines_alloc(char *lineptr[], int maxlines)
 {
+  int getlinealt(char *, int);
+  char *alloc(int);
+
   int len;
   char *p, line[MAXLEN];
   int nlines = 0;
