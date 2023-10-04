773580eda408327bf9654215585cb07635b2da9b
teeaychem <ben@sparkes.uk.net>
2023-10-04 09:34:20 +0100

    tidying

--- K & R/chapter 5/alloc.c
+++ K & R/chapter 5/alloc.c
@@ -6,7 +6,8 @@ static char *allocp = allocbuf; /* next free position */
 char *alloc(int n) /* return pointer to n characters */
 {
   if (allocbuf + ALLOCSIZE - allocp >= n) { /* it fits */
-    allocp += n; return allocp - n; /* old p */
+    allocp += n;
+    return allocp - n; /* old p */
   }
   else /* not enough room */
     return 0;
